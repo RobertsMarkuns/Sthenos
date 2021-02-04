e00b8f52fbed3bf4ffae7577e11c8723982c202f
robertskamarkuns <robertskamarkuns@gmail.com>
2021-02-04 18:13:17 +0200

    Added bottom navigation to exercise activity's

--- app/src/main/java/com/example/sthenos/indoor/pushups/InclinePushUp.java
+++ app/src/main/java/com/example/sthenos/indoor/pushups/InclinePushUp.java
@@ -1,18 +1,28 @@
 package com.example.sthenos.indoor.pushups;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.sthenos.Equipment;
+import com.example.sthenos.IndoorActivity;
+import com.example.sthenos.MainActivity;
+import com.example.sthenos.MobilityActivity;
+import com.example.sthenos.OutdoorActivity;
 import com.example.sthenos.R;
 import com.example.sthenos.RepRangeRandomizer;
 import com.example.sthenos.Stopwatch;
+import com.example.sthenos.UserProfile;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import static com.example.sthenos.R.drawable.inclinepushups;
 
@@ -22,6 +32,7 @@ public class InclinePushUp extends AppCompatActivity {
     TextView exerciseTitle, levelTag, muscleGroupText, descText;
     ImageView exerciseImage;
     Button repRandomizer, stopWatch;
+    BottomNavigationView bottomNavbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +82,43 @@ public class InclinePushUp extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(InclinePushUp.this, Stopwatch.class);
                 startActivity(intent);
+            }
+        });
+
+        //Bottom Navigation Bar
+        bottomNavbar = findViewById(R.id.BottomNavBar);
+
+        bottomNavbar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_indoor:
+                        Intent indoor = new Intent(InclinePushUp.this, IndoorActivity.class);
+                        Toast.makeText(InclinePushUp.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        startActivity(indoor);
+                        break;
+                    case R.id.nav_outdoor:
+                        Intent outdoor = new Intent(InclinePushUp.this, OutdoorActivity.class);
+                        Toast.makeText(InclinePushUp.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        startActivity(outdoor);
+                        break;
+                    case R.id.nav_home:
+                        Intent home = new Intent(InclinePushUp.this, MainActivity.class);
+                        Toast.makeText(InclinePushUp.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        startActivity(home);
+                        break;
+                    case R.id.nav_mobility:
+                        Intent mobility = new Intent(InclinePushUp.this, MobilityActivity.class);
+                        Toast.makeText(InclinePushUp.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        startActivity(mobility);
+                        break;
+                    case R.id.nav_equipment:
+                        Intent equipment = new Intent(InclinePushUp.this, Equipment.class);
+                        Toast.makeText(InclinePushUp.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        startActivity(equipment);
+                        break;
+                }
+                return true;
             }
         });
     }
